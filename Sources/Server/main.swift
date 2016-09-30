c80612194a61a1daf5a8bb1286f61d56598f26a2
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-09-30 11:10:37 -0500

    Added json serialization as well

--- Sources/Server/main.swift
+++ Sources/Server/main.swift
@@ -5,18 +5,34 @@ import Web
 let router = Router()
 
 // Handle HTTP GET requests to /
-router.get("/") {
+router.get("/protobuf") {
     request, response, next in
     
     let b = BookInfo(id: 303, title: "The Odyssey", author: "Homer")
     
     let data = try b.serializeProtobuf()
+    let jsonBook = try b.serializeJSON()
     
     response.send(data: data)
+    
     // response.send("Hello, World!")
 
     next()
 }
+
+router.get("/json") {
+    request, response, next in
+    
+    let b = BookInfo(id: 303, title: "The Odyssey", author: "Homer")
+    
+    let jsonBook = try b.serializeJSON()
+
+    response.send(jsonBook)
+    
+    next()
+
+}
+
 
 // Add an HTTP server and connect it to the router
 Kitura.addHTTPServer(onPort: 8090, with: router)
